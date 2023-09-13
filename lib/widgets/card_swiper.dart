20ed72d9921fa32f2146050f7ad19dc65fbcff05
EmmanuelSalcido <emmanuelem2000@hotmail.com>
2023-09-13 11:54:05 -0600

    bugs en assets

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 //import 'package:peliculas_2023/widgets/widgets.dart';
 
@@ -6,10 +7,31 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 500,
-      color: Colors.red,
-    );
+    final size = MediaQuery.of(context).size; //
+
+    return SizedBox(
+        width: double.infinity,
+        height: size.height * 0.5,
+        child: Swiper(
+          itemCount: 10,
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.4,
+          itemBuilder: (_, int index) {
+            return GestureDetector(
+              onTap: () =>
+                  Navigator.pushNamed(context, 'details', arguments: ''),
+              child: ClipRRect(
+                //para hacer redondeado
+                borderRadius: BorderRadius.circular(20),
+                child: const FadeInImage(
+                  placeholder: AssetImage('assets/camara.jpg'),
+                  image: NetworkImage(
+                      'https://via.placeholder.com/300x400'), //https://via.placeholder.com/300x400 para que se guarde con el tamañ
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
