1709e47ac743a3689c88a376417d48d6a6b1ab80
EmmanuelSalcido <emmanuelem2000@hotmail.com>
2023-10-24 11:50:23 -0600

    los autores ya se ven

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -6,51 +6,61 @@ import 'package:peliculas_2023/models/movie.dart';
 import 'package:peliculas_2023/models/now_playing_response.dart';
 import 'package:peliculas_2023/models/popular_response.dart';
 
+import '../models/credits_response.dart';
+
 class MoviesProvider extends ChangeNotifier {
   String _baseUrl = 'api.themoviedb.org';
   String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
   String _language = 'es-MX';
 
-  List<Movie> onDisplayMovies = []; //Lista con las películas
+  List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
+  Map<int, List<Cast>> moviesCast = {};
 
   MoviesProvider() {
     getOnDisplayMovies();
     getPopularMovies();
   }
+
   getOnDisplayMovies() async {
-    //aquí se llena la lista de las peliculas
-    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
-      'api_key': _apiKey,
-      'language': _language,
-      'page': '1',
-    });
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
 
     final response = await http.get(url);
     final Map<String, dynamic> decodeData = json.decode(response.body);
     //print(decodeData);
     //print(response.body);
-    final nowPlayingResponse = NowPlayingResponse.fromRawJson(response
-        .body); //Creación de una variable que aprende la respuesta de la petición (instancia)
-    onDisplayMovies = nowPlayingResponse.results;
-    //le notificamos a los widgets que estan escuchando que se cambió la data por lo tanto se tiene que redibujar
-    notifyListeners(); //Actualiza todo
-    //  print(nowPlayingResponse.results[0].title); //
+    final nowPLayingResponse = NowPlayingResponse.fromRawJson(response.body);
+    onDisplayMovies = nowPLayingResponse.results;
+    //Le comunicamos a todos los widgets que estan escuchando que se cambio la data por lo tanto se tienen que redibujar
+    notifyListeners();
+    //print(nowPLayingResponse.results[0].title);
   }
 
   getPopularMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/popular', {
-      'api_key': _apiKey,
-      'language': _language,
-      'page': '1',
-    });
+    var url = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
     final response = await http.get(url);
 
-    final popularResponse = PopularResponse.fromRawJson(response
-        .body); //Creación de una variable que aprende la respuesta de la petición
+    final popularResponse = PopularResponse.fromRawJson(response.body);
 
-    //Destructurar resultado para cambiar página y mantener los datos actuales
+    //Destructurar resultado para cambiar pagina y mantener los actuales
     popularMovies = [...popularMovies, ...popularResponse.results];
     notifyListeners();
+  }
+
+  Future<List<Cast>> getMoviesCast(int movieId) async {
+    if (moviesCast.containsKey(movieId)) return moviesCast[movieId]!;
+
+    var url = Uri.https(_baseUrl, '3/movie/$movieId/credits',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final creditsResponse = CreditsResponse.fromJson(jsonDecode(response.body));
+
+    moviesCast[movieId] = creditsResponse.cast;
+
+    return creditsResponse.cast;
   }
 }
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,62 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas_2023/models/models.dart';
+import 'package:peliculas_2023/widgets/casting_cards.dart';
+
+import '../models/models.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //recibir argumentos
+    //Recibir argumentos de otra pantalla
     final Movie movie = ModalRoute.of(context)?.settings.arguments as Movie;
-
     return Scaffold(
-        body: CustomScrollView(
-      //widget con comportamiento predefinido al hacer scroll
-      slivers: [
-        _CustomAppBar(
-          movie: movie,
-        ),
-        SliverList(
-            delegate: SliverChildListDelegate.fixed([
-          _PosterAndTitle(movie: movie),
-          _Overview(
-            movie: movie,
-          ),
-          _Casting(),
-        ]))
-      ],
-    ));
+      body: CustomScrollView(
+        //Widget con comportamientos predefinidos al scroll
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate.fixed(
+              [
+                _PosterAndTitle(movie: movie),
+                _Overview(),
+                CastingCards(movieId: movie.id)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
 class _CustomAppBar extends StatelessWidget {
-  final Movie movie;
-  const _CustomAppBar({super.key, required this.movie});
+  const _CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      backgroundColor: Colors.cyan,
+      backgroundColor: Colors.indigoAccent,
       expandedHeight: 200,
       floating: false,
       pinned: true,
-      //Widget para que se ajuste al tamaño
+      //wIDGET PARA QUE SE AGUSTE AL TAMAÑO
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        //eliminar el padding
-        titlePadding: const EdgeInsets.all(0),
+        //Eliminar padding
+        titlePadding: EdgeInsets.all(0),
         title: Container(
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           color: Colors.black12,
-          child: Text(
-            movie.title,
+          child: const Text(
+            'movie.title',
             style: TextStyle(fontSize: 18),
           ),
         ),
         background: FadeInImage(
-          placeholder: const AssetImage('assets/loading.gif'),
-          image: NetworkImage(movie.backImage),
+          placeholder: AssetImage('assets/loading.gif'),
+          image: AssetImage('assets/no-image.jpg'),
         ),
       ),
     );
@@ -72,165 +72,74 @@ class _PosterAndTitle extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(top: 20),
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'),
-            image: NetworkImage(movie.fullPosterImg),
-            height: 250,
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage(movie.fullPosterImg),
+              height: 250,
+            ),
           ),
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                movie.title,
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-              Text(
-                movie.originalTitle,
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-              Row(children: [
-                Icon(
-                  Icons.star_outline,
-                  size: 20,
-                  color: Color.fromARGB(255, 207, 188, 16),
-                ),
-                SizedBox(
-                  width: 5,
+          SizedBox(width: 20),
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'movie.title',
+                  style: TextStyle(fontSize: 30),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
                 ),
                 Text(
-                  movie.voteAverage.toString(),
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
+                  'movie.titleOriganl',
+                  style: TextStyle(fontSize: 18),
                   textAlign: TextAlign.center,
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
-              ])
-            ],
-          ),
-        )
-      ]),
-    );
-  }
-}
-
-class _Overview extends StatelessWidget {
-  final Movie movie;
-  _Overview({super.key, required this.movie});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Text(
-        movie.overview,
-        textAlign: TextAlign.justify,
-        style: TextStyle(fontSize: 15),
-      ),
-    );
-  }
-}
-
-class _Casting extends StatelessWidget {
-  const _Casting({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size; //Variable para el tamaño
-    return Container(
-      width: double.infinity,
-      height: size.height * 0.35,
-      color: Colors.indigo,
-      child: Column(
-        //una columna
-        crossAxisAlignment: CrossAxisAlignment.start, //alineacion
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-                //Es para tener una separacion en ambos lados del mismo tamaño (horizontal y vertical)
-                horizontal: 20,
-                vertical: 10),
-            child: Text(
-              //Título de la seccion
-              'Casting: ',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold, //en negritas
-                  color: Colors.white),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_outline,
+                      size: 20,
+                      color: Colors.blueAccent,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'movie.voteAverage',
+                      style: TextStyle(fontSize: 15),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    )
+                  ],
+                )
+              ],
             ),
-          ),
-          Expanded(
-              child: ListView.builder(
-            scrollDirection: Axis.horizontal, //para moverlo a los lados
-            itemCount: 20, //que solo se vean 20
-            itemBuilder: (_, int index) =>
-                const _CastPoster(), //se llama a la lista/contenedor de la foto y nombre
-          ))
+          )
         ],
       ),
     );
   }
 }
 
-class _CastPoster extends StatelessWidget {
-  //el "_" en el nombre sirve para hacerlo privado
-  const _CastPoster({super.key});
+class _Overview extends StatelessWidget {
+  const _Overview({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      //Contenedor en el que va la foto y el nombre
-      width: 130,
-      height: 250,
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(children: [
-        GestureDetector(
-            //Para que detecte lo que pasa con la imagen
-            // onTap: () => Navigator.pushNamed(context, 'details',
-            //     arguments:
-            //         ''), //El evento, en este caso hace que se muestre otra pantalla
-            child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: const FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'),
-            image: AssetImage(
-                'assets/no-image.jpg'), //aqui va la foto del actor/actriz
-            width: 130,
-            height: 150,
-          ),
-        )),
-        const SizedBox(
-          //cajita para el nombre del actor
-          height: 5,
-        ),
-        const Text(
-          'Nombre del actor o actriz',
-          maxLines: 2,
-          overflow:
-              TextOverflow.ellipsis, //Para que no salga que es mucho texto
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-      ]),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      child: const Text(
+        'Exercitation sint velit irure occaecat sint magna quis non sunt deserunt. Occaecat id pariatur velit labore tempor. Ex ea do ipsum eiusmod ad laboris minim. Velit aliquip in ex aliquip nulla reprehenderit dolor occaecat proident dolor dolore consectetur laboris. Nisi exercitation labore consectetur cillum quis enim enim veniam culpa laborum.',
+        textAlign: TextAlign.justify,
+        style: TextStyle(fontSize: 15),
+      ),
     );
   }
 }
